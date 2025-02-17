93b2c08a82890ff3367980d91be051203eb3272c
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-02-17 23:15:30 +0000

    I've adjusted the print function to make the result more visible

--- challengerBegin/challenger006.py
+++ challengerBegin/challenger006.py
@@ -8,4 +8,8 @@ num= int(input("Type one number: "))
 num2 = num*2
 num3 = num*3
 num4 = num**(1/2)
-print("the number you entered was", num, "the double of this number is ", num2, "the triple is", num3, "and the square root is ",num4)
+
+print ("the number you entered was {} the double of this number is {}".format(num, num2))
+print ("the triple is igual to {} and the square root is {:.2f}".format(num3, num4))
+
+# print("the number you entered was", num, "the double of this number is ", num2, "the triple is", num3, "and the square root is ",num4)
