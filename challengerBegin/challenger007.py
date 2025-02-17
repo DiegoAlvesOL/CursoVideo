4c1c8ad2e30f7ddba33242f89e6c2dc6d8534553
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-02-17 23:17:43 +0000

    I changed the data type in the input function from int -> to float.

--- challengerBegin/challenger007.py
+++ challengerBegin/challenger007.py
@@ -4,9 +4,12 @@ print("="*17)
 print("the purpose of this programme is to receive two marks form a studant and calculate their average")
 print("="*17)
 
-mark1 = int(input("Type your firs mark: "))
-mark2 = int(input("Type your second mark: "))
+mark1 = float(input("Type your firs mark: "))
+mark2 = float(input("Type your second mark: "))
 averageMark = (mark1+mark2)/2
+
+print("the average between {:.1f} and {:.1f} is {:.1f}".format(mark1, mark2, averageMark))
+
 
 print ("Your average is: ", averageMark)
 
